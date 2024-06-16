06526b63b67036b5fee4055dde2d51650386b810
LubomirJagos42 <lubomir.jagos.42@gmail.com>
2024-06-16 19:37:46 +0200

    Added login module which is now taking tokens from server, this logic is defnitely not good but it's running now and I can focus on IDE logic.

---
+++ GraphLang IDE/LoginClient.py
@@ -0,0 +1,149 @@
+import requests
+import hashlib
+import time
+
+URL = 'http://localhost/GraphLangServerApp/?q=userLogin'
+
+#USERNAME = "lubomir.jagos@hidden-mail.com"
+#USERPASSWORD = hashlib.md5('heslo123'.encode()).hexdigest()
+
+def loginUsingNamePassword(USERNAME = "", PASSWORD = ""):
+    try:
+        USERNAME
+        if (not USERPASSWORD):
+            raise "";
+    except:
+        USERNAME = input("username (email): ")
+    
+    try:
+        
+        if (not USERPASSWORD):
+            raise "";
+    except:
+        USERPASSWORD = input("password: ")
+        USERPASSWORD = hashlib.md5(USERPASSWORD.encode()).hexdigest()
+
+    #
+    #   login - using username and password
+    #
+    myData = {}
+    myData['username'] = USERNAME
+    myData['password'] = USERPASSWORD
+
+    x = requests.post(URL, data = myData)
+
+    response = {}
+    for variable in x.text.split("&"):
+        item = variable.split("=")
+        response[item[0]] = item[1]
+
+    return response, USERNAME, USERPASSWORD
+
+def loginUsingNameToken(USERNAME = "", USERPASSWORD = "", USERTOKEN = ""):
+    try:
+        USERNAME
+        if (not USERPASSWORD):
+            raise "";
+    except:
+        USERNAME = input("username (email): ")
+    
+    try:
+        USERPASSWORD
+        if (not USERPASSWORD):
+            raise "";
+    except:
+        USERPASSWORD = input("password: ")
+        USERPASSWORD = hashlib.md5(USERPASSWORD.encode()).hexdigest()
+
+    myData = {}
+    myData['username'] = USERNAME
+    
+    token = USERPASSWORD
+    token += response['token']    
+    myData['token'] = hashlib.md5(token.encode()).hexdigest()
+
+    x = requests.post(URL, data = myData)
+
+    response = {}
+    for variable in x.text.split("&"):
+        item = variable.split("=")
+        response[item[0]] = item[1]
+
+    return response
+
+
+
+if __name__ == "__main__":
+
+    try:
+        USERNAME
+        if (not USERPASSWORD):
+            raise "";
+    except:
+        USERNAME = input("username (email): ")
+    
+    try:
+        
+        if (not USERPASSWORD):
+            raise "";
+    except:
+        USERPASSWORD = input("password: ")
+        USERPASSWORD = hashlib.md5(USERPASSWORD.encode()).hexdigest()
+
+    print()
+    
+    #
+    #   login - using username and password
+    #
+    myData = {}
+    myData['username'] = USERNAME
+    myData['password'] = USERPASSWORD
+
+    print(f"#REQUEST LOGIN username, password - result:");
+    x = requests.post(URL, data = myData)
+    print(x.text)    
+
+    response = {}
+    for variable in x.text.split("&"):
+        item = variable.split("=")
+        response[item[0]] = item[1]
+    print(response)
+    print()
+
+    print("PRESS ENTER")
+    a = input()
+    print()
+
+    #
+    #   login - using username and token
+    #
+    myData = {}
+    myData['username'] = USERNAME
+    
+    token = USERPASSWORD
+    token += response['token']    
+    myData['token'] = hashlib.md5(token.encode()).hexdigest()
+
+
+    print(f"#REQUEST LOGIN username, token - result:");
+    print(f"token: {token}")
+    print(f"post token: {myData['token']}")
+
+    x = requests.post(URL, data = myData)
+    print()
+    print(x.text)    
+
+    response = {}
+    for variable in x.text.split("&"):
+        item = variable.split("=")
+        response[item[0]] = item[1]
+    print(response)
+    print()
+
+
+
+
+
+
+
+
--- GraphLang IDE/uploadNodesToServer.py
+++ GraphLang IDE/uploadNodesToServer.py
@@ -2,16 +2,29 @@ import requests
 import json
 import os
 import time
+import math
+import LoginClient as login
+import hashlib
+import re
 
 import createWebPage as localNodesHelper
 
 URL = 'http://localhost/GraphLangServerApp/?q=uploadNodesToServer'
-NODE_OWNER = 2
-NODE_PROJECT = 47
+
 NODE_LANGUAGE = "C/C++"
 NODE_ISHIDDEN = 0
 
 if __name__ == "__main__":
+    loginInfo, username, passwordMD5 = login.loginUsingNamePassword()
+    usertoken = loginInfo['token']
+
+    if (loginInfo['isLogged'] == 0):
+        print("user not logged, exit")
+        exit(0)
+
+    NODE_PROJECT = input("project id: ")
+    NODE_PROJECT = abs(int(NODE_PROJECT))
+    
     start_time = time.time()
     
     print("======== START SEARCHING LOCAL FILE SYSTEM FOR NODES ==========")
@@ -23,13 +36,18 @@ if __name__ == "__main__":
         print(nodeToUpload)
         
         myData = {}
+        
+        myData['username'] = username
+        token = passwordMD5
+        token += usertoken
+        myData['token'] = hashlib.md5(token.encode()).hexdigest()
+    
         myData['nodeName'] = nodeToUpload[2]
         
         nodeFilePtr = open(nodeToUpload[1], 'rb')
-        NODE_CONTENT = nodeFilePtr.read()
-        
+        NODE_CONTENT = nodeFilePtr.read()   
         myData['nodeContent'] = NODE_CONTENT.hex()
-        myData['nodeOwner'] = NODE_OWNER
+        
         myData['projectId'] = NODE_PROJECT
         myData['nodeLanguage'] = NODE_LANGUAGE
         myData['nodeDir'] = nodeToUpload[0].replace('\\', '/')
@@ -40,7 +58,14 @@ if __name__ == "__main__":
 
         print(f"#REQUEST 1 - result:\n");
         x = requests.post(URL, data = myData)
-        print(x.text)    
+        print(x.text)
+        
+        #
+        # get new token from response
+        #
+        result = re.search(r"token:'([a-zA-Z0-9]+)'", x.text)
+        usertoken = result.group(1)
+        print(f"token found: {usertoken}")
 
     #
     #   DIFFERENT METHOD POST DATA - using json - In progress... not running now
